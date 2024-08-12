85d96c4138efd363e66060ca38365cc84bdbd60d
vanja.culum <vanja.culum@geneza.com>
2024-08-12 09:20:24 +0200

    test

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,3 @@
+export default function Invoices(props) {
+  return <div>invoices</div>;
+}
---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+import "@/app/ui/global.css";
+import { inter } from "./ui/fonts";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html className={`${inter.className} antialiased`} lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
